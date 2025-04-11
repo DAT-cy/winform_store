dc1c1a682167315baefdf4662ad7a46026b8a5b9
tuan <dat224k@gmail.com>
2025-04-11 16:58:04 +0700

    done

--- FastFoodProject/SignUp.Designer.cs
+++ FastFoodProject/SignUp.Designer.cs
@@ -90,7 +90,7 @@
             lblWelcome.Name = "lblWelcome";
             lblWelcome.Size = new Size(220, 37);
             lblWelcome.TabIndex = 1;
-            lblWelcome.Text = "Chao Tuấn";
+            lblWelcome.Text = "Chao Anh Minh";
             lblWelcome.TextAlign = ContentAlignment.MiddleCenter;
             // 
             // picLogo
